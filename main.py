dabf94cb1f09d9a7acc42c8f39b673fa5d0276b8
TheGhostOfInky <theghostofinky@gmail.com>
2023-06-26 18:13:54 +0100

    Minor reliability and UX improvements + added extra methods to database driver class

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ except ImportError:
 T = TypeVar("T")
 
 
-def batched(iterable: Iterable[T], n: int) -> Generator[tuple[T, ...], None, None]:
+def batched(iterable: Iterable[T], n: int) -> Generator[list[T], None, None]:
     it = iter(iterable)
-    while (batch := tuple(islice(it, n))):
+    while batch := list(islice(it, n)):
         yield batch
 
 
@@ -24,25 +24,15 @@ DIR_NAME = os.path.dirname(os.path.realpath(__file__))
 with open(DIR_NAME + "/config.toml", "rb") as f:
     config = tomllib.load(f)
 
-db = DB(DIR_NAME + "/time.db")
-
+# Global variables defined at startup
+db: DB
 last_time: datetime.datetime
+webhook: Webhook
 
-if lt := db.get("last-time"):
-    last_time = datetime.datetime.fromisoformat(lt)
-else:
-    last_time = datetime.datetime.now(tz=datetime.timezone.utc)
-
-user_name = config["user-name"]
+user_name: str = config["user-name"]
 limit: int = config.get("limit", 20)
 delay: int = config.get("delay", 550)
 rand_range: int = config.get("rand-range", 100)
-
-webhook = Webhook(
-    config["webhook-url"], user_name,
-    config.get("pfp", ""),
-    config.get("ping-roles", [])
-)
 
 
 def post() -> None:
@@ -52,20 +42,20 @@ def post() -> None:
     newer = tweets.newer_than(last_time)
 
     now = datetime.datetime.now(tz=datetime.timezone.utc)
-    last_time = now
+
     db.set("last-time", now.isoformat())
+    last_time = now
 
     if newer:
         for tw in batched(newer, 10):
-            webhook.post_to_webhook(list(tw))
+            webhook.post_to_webhook(tw)
 
-        logging.info(f"Posted {len(newer)} tweets")
+        logging.info(f"Posted {len(newer)} tweet(s)")
     else:
         logging.info("No new tweets to post")
 
 
 def main(scheduler: sched.scheduler) -> None:
-
     l_delay = delay + randint(0, rand_range)
     scheduler.enter(l_delay, 1, main, (scheduler,))
 
@@ -83,6 +73,21 @@ if __name__ == "__main__":
         level=logging.INFO
     )
 
+    db = DB(DIR_NAME + "/time.db")
+
+    if lt := db.get("last-time"):
+        last_time = datetime.datetime.fromisoformat(lt)
+        logging.info(f"Loaded {lt} from database")
+    else:
+        last_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        logging.info("No previous time found in database, defaulting to current time")
+
+    webhook = Webhook(
+        config["webhook-url"], user_name,
+        config.get("pfp", ""),
+        config.get("ping-roles", [])
+    )
+
     scheduler = sched.scheduler()
     scheduler.enter(2, 1, main, (scheduler, ))
 
@@ -90,5 +95,9 @@ if __name__ == "__main__":
         scheduler.run()
     except (KeyboardInterrupt, SystemExit):
         logging.info("Got signal to exit")
+
         for x in scheduler.queue:
             scheduler.cancel(x)
+
+    del db
+    exit(0)
--- sqlite3_dbdriver.py
+++ sqlite3_dbdriver.py
@@ -9,7 +9,7 @@ class DB:
     def __init__(self, path: str) -> None:
         self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS KeyVal (Key TEXT, Value TEXT)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS KeyVal (Key TEXT PRIMARY KEY, Value TEXT)")
 
     @overload
     def get(self, key: str) -> Optional[str]:
@@ -34,14 +34,6 @@ class DB:
         else:
             return default
 
-    def __getitem__(self, key: str) -> str:
-        val = self.get(key)
-
-        if val is None:
-            raise KeyError(key)
-        else:
-            return val
-
     def set(self, key: str, value: str) -> None:
         if not isinstance(key, str):
             raise TypeError("Only string keys are allowed")
@@ -49,17 +41,8 @@ class DB:
         if not isinstance(value, str):
             raise TypeError("Only string values are allowed")
 
-        exists = self.cur.execute("SELECT EXISTS(SELECT 1 FROM KeyVal WHERE Key=?)", (key,))
-
-        if exists.fetchone()[0]:
-            self.cur.execute("UPDATE KeyVal SET Value = ? WHERE Key=?", (value, key))
-        else:
-            self.cur.execute("INSERT INTO KeyVal VALUES(?,?)", (key, value))
-
+        self.cur.execute("INSERT OR REPLACE INTO KeyVal (Key, Value) VALUES (?, ?)", (key, value))
         self.con.commit()
-
-    def __setitem__(self, key: str, value: str) -> None:
-        self.set(key, value)
 
     def pop(self, key: str, default: Optional[str] = None) -> str:
         if not isinstance(key, str):
@@ -77,5 +60,43 @@ class DB:
             else:
                 return default
 
+    def keys(self) -> list[str]:
+        keys_in_db = self.cur.execute("SELECT Key FROM KeyVal")
+        return [cast(str, x[0]) for x in keys_in_db.fetchall()]
+
+    def values(self) -> list[str]:
+        values_in_db = self.cur.execute("SELECT Value FROM KeyVal")
+        return [cast(str, x[0]) for x in values_in_db.fetchall()]
+
+    def items(self) -> list[tuple[str, str]]:
+        kv_in_db = self.cur.execute("SELECT Key, Value FROM KeyVal")
+        return [cast(tuple[str, str], x) for x in kv_in_db.fetchall()]
+
+    def clear(self) -> None:
+        self.cur.execute("DELETE FROM KeyVal")
+        self.con.commit()
+
+    def __getitem__(self, key: str) -> str:
+        val = self.get(key)
+
+        if val is None:
+            raise KeyError(key)
+        else:
+            return val
+
+    def __setitem__(self, key: str, value: str) -> None:
+        self.set(key, value)
+
     def __delitem__(self, key: str) -> None:
         self.pop(key)
+
+    def __contains__(self, key: str) -> bool:
+        exists = self.cur.execute("SELECT EXISTS(SELECT 1 FROM KeyVal WHERE Key=?)", (key,))
+        return exists.fetchone()[0] == 1
+
+    def __len__(self) -> int:
+        size = self.cur.execute("SELECT COUNT(*) FROM KeyVal")
+        return cast(int, size.fetchone()[0])
+
+    def __del__(self) -> None:
+        self.con.close()
